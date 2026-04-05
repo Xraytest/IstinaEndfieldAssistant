4ac2f1ecf6c7f6a459d37ddc34daadb4a39559c9
xray4668 <chengqing.2008@qq.com>
2026-04-05 17:51:51 +0800

    fix:remanage touch

--- core/__init__.py
+++ core/__init__.py
@@ -3,12 +3,19 @@
 from .logger import init_logger, get_logger, LogCategory, LogLevel
 from .adb_manager import ADBDeviceManager
 from .screen_capture import ScreenCapture
-from .touch import TouchExecutor, TouchAdapter, MaaFwTouchExecutor, MaaFwTouchConfig
+from .touch import (
+    TouchManager, TouchDeviceType,
+    TouchExecutor, MaaFwTouchExecutor, MaaFwTouchConfig,
+    MaaFwWin32Executor, MaaFwWin32Config, TouchConfig
+)
 from .communication import ClientCommunicator
 
 __all__ = [
     'init_logger', 'get_logger', 'LogCategory', 'LogLevel',
     'ADBDeviceManager', 'ScreenCapture',
-    'TouchExecutor', 'TouchAdapter', 'MaaFwTouchExecutor', 'MaaFwTouchConfig',
+    # 触控系统
+    'TouchManager', 'TouchDeviceType',
+    'TouchExecutor', 'MaaFwTouchExecutor', 'MaaFwTouchConfig',
+    'MaaFwWin32Executor', 'MaaFwWin32Config', 'TouchConfig',
     'ClientCommunicator'
 ]
--- core/touch/__init__.py
+++ core/touch/__init__.py
@@ -1,11 +1,27 @@
-"""触控子系统"""
+"""触控子系统 - 使用MaaFramework库管理触控
 
-from .touch_executor import TouchExecutor
-from .touch_adapter import TouchExecutor as TouchAdapter  # 别名兼容
-from .touch_adapter import MaaTouchConfig
+可选触控方式：
+- 安卓设备: MaaTouch (通过MaaFramework AdbController)
+- PC前台: Win32Controller (通过MaaFramework Win32Controller)
+
+推荐使用TouchManager统一管理触控操作。
+"""
+
+from .touch_manager import TouchManager, TouchDeviceType
 from .maafw_touch_adapter import MaaFwTouchExecutor, MaaFwTouchConfig
+from .maafw_win32_adapter import MaaFwWin32Executor, MaaFwWin32Config
+
+# 兼容别名
+TouchExecutor = MaaFwTouchExecutor
+TouchConfig = MaaFwTouchConfig
 
 __all__ = [
-    'TouchExecutor', 'TouchAdapter', 'MaaTouchConfig',
-    'MaaFwTouchExecutor', 'MaaFwTouchConfig'
+    # 统一触控管理器
+    'TouchManager', 'TouchDeviceType',
+    # Android触控
+    'MaaFwTouchExecutor', 'MaaFwTouchConfig',
+    # PC触控
+    'MaaFwWin32Executor', 'MaaFwWin32Config',
+    # 兼容别名
+    'TouchExecutor', 'TouchConfig'
 ]
